330e65829e7662251b7eb57d45aee19a497bb8db
Thu Kha Aung <thukha_aung@truemoneymyanmar.com>
2018-12-15 13:25:03 +0630

    First Commit

---
+++ docker-spring-boot/src/main/java/com/truemoney/docker/dockerspringboot/DockerSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.truemoney.docker.dockerspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerSpringBootApplication.class, args);
+	}
+
+}
+
---
+++ docker-spring-boot/src/main/java/com/truemoney/docker/dockerspringboot/resource/HelloResource.java
@@ -0,0 +1,15 @@
+package com.truemoney.docker.dockerspringboot.resource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest/docker/hello")
+public class HelloResource {
+
+	@GetMapping
+	public String hello() {
+		return "Hello Youtube";
+	}
+}
